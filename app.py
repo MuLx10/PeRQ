5dc68096fe09f966b2fd2f343ee68b3e40deeb3b
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-06-28 23:25:35 +0530

    GoogleAssistantHorroscope

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify, url_for
 import aiml
 import os
 from chatbot import Chatbot
+from utils import get_json_resp
+import json
 
 app = Flask(__name__)
 
@@ -30,6 +32,50 @@ def ask():
             bot_response = bot.atomic_router(message)
             return jsonify({'status':'OK','answer':bot_response})
 
+@app.route("/horro", methods=['POST','GET'])
+def horo():
+    data = json.loads(request.data)
+
+    sunsign = str(data['queryResult']['parameters']['astroSign']).lower()
+    day = str(data['queryResult']['parameters']['day']).lower()
+    print(sunsign,day)
+    resp = get_json_resp(sunsign,day)
+    return jsonify(resp)
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
+
+
+# {
+#   "responseId": "a5b77534-10c6-467c-8ae6-5ee479dfce61",
+#   "queryResult": {
+#     "queryText": "What's today's horoscope for Leo?",
+#     "parameters": {
+#       "astroSign": "Leo",
+#       "day": "today"
+#     },
+#     "allRequiredParamsPresent": true,
+#     "fulfillmentText": "Hi Leo today",
+#     "fulfillmentMessages": [{
+#       "text": {
+#         "text": ["Hi Leo today"]
+#       }
+#     }],
+#     "intent": {
+#       "name": "projects/tarrot-99e8d/agent/intents/428e1c41-0ab0-4242-8114-d7c7aa26c1b5",
+#       "displayName": "actions.intent.GET_HOROSCOPE"
+#     },
+#     "intentDetectionConfidence": 1.0,
+#     "diagnosticInfo": {
+#     },
+#     "languageCode": "en"
+#   },
+#   "originalDetectIntentRequest": {
+#     "payload": {
+#     }
+#   },
+#   "session": "projects/tarrot-99e8d/agent/sessions/fef2289f-7493-f0c5-765a-9e34e3bdffa2"
+# }
---
+++ utils.py
@@ -0,0 +1,56 @@
+import urllib, json
+from copy import copy
+fulfillmentMessages = {
+                        "fulfillmentMessages": [
+                            {
+                              "card": {
+                                "title": "Leo",
+                                "subtitle": "Life could get really interesting today. At the very least, you're likely to read or hear something that captures your attention and fascinates you. An interesting article could kick off a whole new hobby or course of study! Who knows? As long as you're brave enough to chase down the people, places and subjects that intrigue you, you're taking advantage of today's influence in exactly the right way",
+                                "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                                "buttons": [
+                                  {
+                                    "text": "(c) Kelli Fox, The Astrologer",
+                                    "postback": "http://sandipbgt.com/api/horoscope/"
+                                  }
+                                ]
+                              }
+                            },
+                            {
+                              "text": {
+                                "text": [
+                                  "Life could get really interesting today. At the very least, you're likely to read or hear something that captures your attention and fascinates you. An interesting article could kick off a whole new hobby or course of study! Who knows? As long as you're brave enough to chase down the people, places and subjects that intrigue you, you're taking advantage of today's influence in exactly the right way"
+                                ]
+                              }
+                            }
+                          ],
+                          "outputContexts": [
+                            {
+                              "name": "projects/tarrot-99e8d/agent/sessions/fef2289f-7493-f0c5-765a-9e34e3bdffa2/contexts/HoroScope",
+                              "lifespanCount": 2,
+                              "parameters": {
+                                "astroSign": "Leo"
+                              }
+                            }
+                          ]
+                        }
+
+url = 'http://sandipbgt.com/theastrologer/api/horoscope/'
+
+def fetch_horoscope(sunsign,day):
+  try:
+    response = urllib.urlopen(url+'/'+sunsign+'/'+day)
+    data = json.loads(response.read())
+  except:
+    response = urllib.request.urlopen(url+'/'+sunsign+'/'+day)
+    data = json.loads(response.read().decode())
+  return str(data['horoscope']), str(data['meta']['mood'])
+
+
+def get_json_resp(sunsign,day):
+  resp = copy(fulfillmentMessages)
+  horoscope, mood = fetch_horoscope(sunsign,day)
+  resp['fulfillmentMessages'][0]['card']['title'] = sunsign.upper()
+  resp['fulfillmentMessages'][0]['card']['subtitle'] = horoscope
+  resp['fulfillmentMessages'][0]['card']['buttons'][0]['postback']+=sunsign+'/'+day
+  resp['fulfillmentMessages'][1]['text']['text'] = [horoscope]
+  return resp
