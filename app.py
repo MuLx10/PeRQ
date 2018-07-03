9d40e0db2d25e125816c6efa34848fc513c28035
Mehul Kumar Nirala <mehulkumarnirala@iitkgp.ac.in>
2018-07-03 13:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def horo():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=not True)
 
 
 
